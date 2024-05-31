6093e12bdf7a90db461d2b4455f118f2729ca42e
Shuvosarker4 <shuvo01797451@gmail.com>
2024-05-31 16:38:13 +0600

    student modules created

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
-import express from "express";
+import express, { application } from "express";
 import cors from "cors";
+import { StudentRoutes } from "./modules/students/student.routes";
 const app = express();
 const port = 5000;
 //parsers
 app.use(express.json());
 app.use(cors());
+
+//application routes
+app.use("/api/v1/students", StudentRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
