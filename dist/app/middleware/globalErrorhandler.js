0f89979be09e8ef7dd37414757120218eba409f1
Shuvosarker4 <shuvo01797451@gmail.com>
2024-06-03 01:10:32 +0600

    some error handling

--- dist/app/middleware/globalErrorhandler.js
+++ dist/app/middleware/globalErrorhandler.js
@@ -3,14 +3,50 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const http_status_1 = __importDefault(require("http-status"));
+const zod_1 = require("zod");
+const zodErrors_1 = __importDefault(require("../errors/zodErrors"));
+const validationError_1 = __importDefault(require("../errors/validationError"));
+const handleCastError_1 = __importDefault(require("../errors/handleCastError"));
+const handleDuplicateError_1 = __importDefault(require("../errors/handleDuplicateError"));
 const globalErrorHandler = (err, req, res, next) => {
-    const statusCode = 500;
-    const message = err.message || "Something went wrong";
-    return res.status(http_status_1.default.BAD_REQUEST).json({
+    let statusCode = 500;
+    let message = "Something went wrong!";
+    let errorSources = [
+        {
+            path: "",
+            message: "Something went wrong",
+        },
+    ];
+    if (err instanceof zod_1.ZodError) {
+        const simplifiedError = (0, zodErrors_1.default)(err);
+        statusCode = simplifiedError === null || simplifiedError === void 0 ? void 0 : simplifiedError.statusCode;
+        message = simplifiedError === null || simplifiedError === void 0 ? void 0 : simplifiedError.message;
+        errorSources = simplifiedError === null || simplifiedError === void 0 ? void 0 : simplifiedError.errorSources;
+    }
+    else if ((err === null || err === void 0 ? void 0 : err.name) === "ValidationError") {
+        const simplifiedError = (0, validationError_1.default)(err);
+        statusCode = simplifiedError === null || simplifiedError === void 0 ? void 0 : simplifiedError.statusCode;
+        message = simplifiedError === null || simplifiedError === void 0 ? void 0 : simplifiedError.message;
+        errorSources = simplifiedError === null || simplifiedError === void 0 ? void 0 : simplifiedError.errorSources;
+    }
+    else if ((err === null || err === void 0 ? void 0 : err.name) === "CastError") {
+        const simplifiedError = (0, handleCastError_1.default)(err);
+        statusCode = simplifiedError === null || simplifiedError === void 0 ? void 0 : simplifiedError.statusCode;
+        message = simplifiedError === null || simplifiedError === void 0 ? void 0 : simplifiedError.message;
+        errorSources = simplifiedError === null || simplifiedError === void 0 ? void 0 : simplifiedError.errorSources;
+    }
+    else if ((err === null || err === void 0 ? void 0 : err.code) === 11000) {
+        const simplifiedError = (0, handleDuplicateError_1.default)(err);
+        statusCode = simplifiedError === null || simplifiedError === void 0 ? void 0 : simplifiedError.statusCode;
+        message = simplifiedError === null || simplifiedError === void 0 ? void 0 : simplifiedError.message;
+        errorSources = simplifiedError === null || simplifiedError === void 0 ? void 0 : simplifiedError.errorSources;
+    }
+    //ultimate return
+    return res.status(statusCode).json({
         success: false,
         message,
-        error: err,
+        errorSources,
+        // stack: config.NODE_ENV === "development" ? err?.stack : null,
     });
 };
 exports.default = globalErrorHandler;
